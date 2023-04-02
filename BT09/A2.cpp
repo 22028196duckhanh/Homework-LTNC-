99ab2457a2c81af055cd3c01e456669d6fd0245c
khanhnd1812 <124419616+khanhnd1812@users.noreply.github.com>
2023-04-02 08:04:54 +0700

    Update A2.cpp

--- BT09/A2.cpp
+++ BT09/A2.cpp
@@ -7,7 +7,7 @@ int main(){
     delete p;
     *p2 = 100;
     cout << *p2;
-    delete p2;
+    delete p2;  // lỗi ở đây vì lệnh delete phải xóa 1 con trỏ được cấp phát động new mà p2 thì không phải nên hành vi này là không xác định
 
     return 0;
 }
